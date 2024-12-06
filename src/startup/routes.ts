43dc21101ca6bf75dbe96b3463a8b34134953190
muhammaddawoodsaeed <dawoodsaeed.work@gmail.com>
2024-12-06 12:29:44 +0500

    Initial Setup has been done

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
+import userRouter from "../routes/User";
 
 export default function routes(app: Express) {
   app.use(express.json());
+  app.use("/api/user", userRouter);
 }
